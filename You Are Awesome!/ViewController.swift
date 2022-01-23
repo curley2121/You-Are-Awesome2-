e3b31c0204396a340bfd0c2ff1186f7c410af423
Sean Curley <curleysc@bc.edu>
2022-01-23 17:35:29 -0500

    2.3 commit

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("view loaded")
+        messageLabel.text = "Fabulous!"
     }
 
-
+    @IBAction func messageButtonPress(_ sender: UIButton) {
+        print("pressed")
+        messageLabel.text = "You are Awesome!"
+    }
+    
+    @IBAction func otherButtonPress(_ sender: UIButton) {
+        print("other pressed")
+        messageLabel.text = "You are Great!"
+        
+    }
 }
 
